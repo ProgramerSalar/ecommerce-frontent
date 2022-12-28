7f32ba95e29c9892ec96a76d93b790eb967d3360
manish <manishkumar60708090@gmail.com>
2022-12-28 19:14:14 +0530

    ant design navigation

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router-dom";
 import Login from "./pages/auth/Login";
 import Register from "./pages/auth/Register";
 import Home from "./pages/Home";
+import Header from "./components/nav/Header";
 
 const App = () => {
   return (
-    <Routes>
-      <Route exact path="/" element={<Home />} />
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/register" element={<Register />} />
-    </Routes>
+    <>
+      <Header />
+      <Routes>
+        <Route exact path="/" element={<Home />} />
+        <Route exact path="/login" element={<Login />} />
+        <Route exact path="/register" element={<Register />} />
+      </Routes>
+    </>
   );
 };
 
---
+++ src/components/nav/Header.js
@@ -0,0 +1,31 @@
+import React, { useState } from "react";
+import { Menu } from "antd";
+import {
+  MailOutlined,
+  AppstoreOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
+
+const { SubMenu } = Menu;
+
+const Header = () => {
+  const [current, setCurrent] = useState("");
+  const handleClick = () => {
+    //
+  };
+
+  return (
+    <Menu onClick={handleClick} selectedKey={[current]} mode="horizontal">
+      <Menu.Item key="mail" icon={<MailOutlined />}>
+        Home
+      </Menu.Item>
+
+      <SubMenu icon={<SettingOutlined />} title="Register">
+        <Menu.Item key="setting:1">Option 1</Menu.Item>
+        <Menu.Item key="setting:2">Option 2</Menu.Item>
+      </SubMenu>
+    </Menu>
+  );
+};
+
+export default Header;
