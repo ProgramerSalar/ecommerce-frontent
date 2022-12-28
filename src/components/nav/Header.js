26a8517c065af7630421dc6b6b1f9223eaa6d59a
manish <manishkumar60708090@gmail.com>
2022-12-28 19:40:21 +0530

    ant disign Icons

--- src/components/nav/Header.js
+++ src/components/nav/Header.js
@@ -1,28 +1,39 @@
 import React, { useState } from "react";
 import { Menu } from "antd";
 import {
-  MailOutlined,
   AppstoreOutlined,
   SettingOutlined,
+  UserOutlined,
+  UserAddOutlined,
 } from "@ant-design/icons";
 
-const { SubMenu } = Menu;
+const { SubMenu, Item } = Menu;
 
 const Header = () => {
-  const [current, setCurrent] = useState("");
-  const handleClick = () => {
-    //
+  const [current, setCurrent] = useState("home");
+
+  const handleClick = (e) => {
+    // console.log(e.key);
+    setCurrent(e.key);
   };
 
   return (
     <Menu onClick={handleClick} selectedKey={[current]} mode="horizontal">
-      <Menu.Item key="mail" icon={<MailOutlined />}>
+      <Item key="home" icon={<AppstoreOutlined />}>
         Home
-      </Menu.Item>
+      </Item>
 
-      <SubMenu icon={<SettingOutlined />} title="Register">
-        <Menu.Item key="setting:1">Option 1</Menu.Item>
-        <Menu.Item key="setting:2">Option 2</Menu.Item>
+      <Item key="register" icon={<UserAddOutlined />}>
+        Register
+      </Item>
+
+      <Item key="login" icon={<UserOutlined />} className="float-right">
+        Login
+      </Item>
+
+      <SubMenu icon={<SettingOutlined />} title="Username">
+        <Item key="setting:1">Option 1</Item>
+        <Item key="setting:2">Option 2</Item>
       </SubMenu>
     </Menu>
   );
