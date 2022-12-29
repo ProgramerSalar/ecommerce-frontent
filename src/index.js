0074fa3652dcf85fa1df4aff84e55249de6755dd
manish <manishkumar60708090@gmail.com>
2022-12-29 18:52:46 +0530

    setup Redux

--- src/index.js
+++ src/index.js
@@ -3,15 +3,26 @@ import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import "./index.css";
 
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { composeWithDevTools } from "redux-devtools-extension";
+import rootReducer from "./reducers";
+
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
+
+// store
+const store = createStore(rootReducer, composeWithDevTools());
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   // <React.StrictMode>
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
+
   // </React.StrictMode>
 );
 
