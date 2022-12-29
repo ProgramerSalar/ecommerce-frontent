3dcb81618812a71ee350ab3a336fba89b50094ec
manish <manishkumar60708090@gmail.com>
2022-12-29 19:18:29 +0530

    user in Redux state

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
@@ -9,7 +9,33 @@ import Header from "./components/nav/Header";
 import "./App.css";
 import RegisterComplete from "./pages/auth/RegisterComplete";
 
+import { auth } from "./firebase";
+import { useDispatch } from "react-redux";
+
 const App = () => {
+  const dispatch = useDispatch();
+
+  // to check firebase auth state
+  useEffect(() => {
+    const unsubscribe = auth.onAuthStateChanged(async (user) => {
+      if (user) {
+        const idTokenResult = await user.getIdTokenResult();
+        console.log("user", user);
+
+        dispatch({
+          type: "LOGGED_IN_USER",
+          payload: {
+            email: user.email,
+            token: idTokenResult.token,
+          },
+        });
+      }
+    });
+
+    // cleanup
+    return () => unsubscribe();
+  }, []);
+
   return (
     <>
       <Header />
